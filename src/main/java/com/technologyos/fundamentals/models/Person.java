be74b190eb7075d8eca66d7bfd0ca5e2ee69f787
ArmandoSJ <just-jauregui@hotmail.com>
2025-08-06 19:59:32 -0600

    add person object

---
+++ src/main/java/com/technologyos/fundamentals/models/Person.java
@@ -0,0 +1,18 @@
+package com.technologyos.fundamentals.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+   private String name;
+   private Integer age;
+   private Double height;
+
+   public static Integer howMany(Person... people){
+      return people.length;
+   }
+}
